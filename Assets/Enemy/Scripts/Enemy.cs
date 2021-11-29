b509c0e6bd1813e0d4883d58b581605c965278a0
Alice Tarkanmäki Bohman <nd.ag@hotmail.com>
2021-11-29 02:24:46 +0100

    State pattern partly works, can move from idle to wandering. Currently not changing back to idle after not moving.

--- Assets/Enemy/Scripts/Enemy.cs
+++ Assets/Enemy/Scripts/Enemy.cs
@@ -9,24 +9,64 @@ public class Enemy : Unit
     [SerializeField] protected float attackSpeed;
     [SerializeField] Unit targetToAttack;
     [SerializeField] protected Transform target;
-    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] protected NavMeshAgent agent;
     [SerializeField] protected float idleTimer;
+    [SerializeField] protected Transform unitPos;
+    [SerializeField] protected bool isWaiting = true;
+
+    protected enum EnemyState { Idle, Wandering, MoveToTarget, Attacking}
+
+    public Transform GetUnitPos { get { return unitPos; } }
+
+    protected virtual void UpdateState(Transform unitPos)
+    {
+
+    }
+
+    protected virtual void DecideAction(EnemyState state)
+    {
+
+
+        switch(state)
+        {
+            case EnemyState.Idle:
+                Debug.Log("Idle");
+            break;
+
+            case EnemyState.Wandering:
+                Debug.Log("Wandering");
+                Vector3 newPos = GetRandomPosition();
+                agent.SetDestination(newPos);
+                break;
+
+            case EnemyState.MoveToTarget:
+                Debug.Log("Move to target");
+                break;
+
+            case EnemyState.Attacking:
+                Debug.Log("Attacking");
+                break;
+        }
+    }
 
     private void Start()
     {
         if (agent == null)
             agent = GetComponent<NavMeshAgent>();
+
+        UpdateState(GetUnitPos);
     }
 
     private void Update()
     {
-        if(idleTimer > 0)
+        if(idleTimer > 0 && !isWaiting)
         {
             idleTimer -= Time.deltaTime;
+            idleTimer = Mathf.Clamp(idleTimer, 0, Mathf.Infinity);
 
             if(idleTimer <= 0)
             {
-                Debug.Log("Moving to random position");
+                UpdateState(GetUnitPos);
             }
         }
     }
--- Assets/Enemy/Scripts/Zombie.cs
+++ Assets/Enemy/Scripts/Zombie.cs
@@ -4,6 +4,47 @@ using UnityEngine;
 
 public class Zombie : Enemy
 {
+    EnemyState state;
+    
 
+    protected override void UpdateState(Transform unitPos)
+    {
+        if (target != null)
+        {
+            float distanceToTarget = (unitPos.transform.position - target.position).magnitude;
+        }
 
+        switch(state)
+        {
+            case EnemyState.Idle:
+                if (idleTimer == 0 && isWaiting)
+                {
+                    idleTimer = RandomTimer(5, 10);
+                    isWaiting = false;
+                }
+                else if (idleTimer <= 0)
+                {
+                    state = EnemyState.Wandering;
+                    idleTimer = 0;
+                    isWaiting = true;
+                }
+                break;
+            case EnemyState.Wandering:
+                if(agent.velocity.magnitude < 1)
+                {
+                    state = EnemyState.Idle;
+                }
+                break;
+            case EnemyState.MoveToTarget:
+
+                break;
+            case EnemyState.Attacking:
+
+                break;
+
+        }
+
+        DecideAction(state);
+
+    }
 }
